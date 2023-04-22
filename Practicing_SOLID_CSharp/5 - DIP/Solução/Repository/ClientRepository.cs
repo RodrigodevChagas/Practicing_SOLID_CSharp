6eee994fff1a8d4d1f2abdd422fe96f5a2da0af2
Rodrigo Chagas <dyggo.ag22@gmail.com>
2023-04-22 11:44:12 -0300

    implementando solução usando DIP - SOLID

---
+++ Practicing_SOLID_CSharp/5 - DIP/Solução/Repository/ClientRepository.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Practicing_SOLID_CSharp._5___DIP.Solução.Models;
+using Practicing_SOLID_CSharp._5___DIP.Solução.Interfaces;
+
+namespace Practicing_SOLID_CSharp._5___DIP.Solução.Repository
+{
+    public class ClientRepository : IClientRepository
+    {
+        public void AddClient(Client client)
+        {
+            try
+            {
+                using (var cn = new SqlConnection())
+                {
+                    var cmd = new SqlCommand();
+
+                    cn.ConnectionString = "MinhaConnectionString";
+                    cmd.Connection = cn;
+                    cmd.CommandType = CommandType.Text;
+                    cmd.CommandText = "INSERT INTO CLIENTE (NOME, EMAIL CPF, DATACADASTRO) VALUES (@nome, @email, @cpf, @dataCad))";
+
+                    cmd.Parameters.AddWithValue("nome", client.Nome);
+                    cmd.Parameters.AddWithValue("email", client.Email);
+                    cmd.Parameters.AddWithValue("cpf", client.CPF);
+                    cmd.Parameters.AddWithValue("dataCad", client.DataCadastro);
+
+                    cn.Open();
+                    cmd.ExecuteNonQuery();
+                }
+            }
+            catch (Exception ex) 
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }   
+    }
+}
