11bdd6df3a1dedeb388de386c79850ee84da2129
Rodrigo Chagas <dyggo.ag22@gmail.com>
2023-04-22 11:42:47 -0300

    implementando solução usando ISP - SOLID

---
+++ Practicing_SOLID_CSharp/4 - ISP/Solução/Interfaces/ICadastroCliente.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practicing_SOLID_CSharp._4___ISP.Solução.Interfaces
+{
+    internal interface ICadastroCliente
+    {
+        void EnviarEmail();
+        void SalvarBanco();
+        void ValidarDados();
+        
+    }
+}
---
+++ Practicing_SOLID_CSharp/4 - ISP/Solução/Interfaces/ICadastroProduto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practicing_SOLID_CSharp._4___ISP.Solução.Interfaces
+{
+    internal interface ICadastroProduto
+    {
+        void SalvarBanco();
+        void ValidarDados();
+    }
+}
