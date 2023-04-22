1a1ddf21f49cd2fe7c54210ef7cd1d8bda0cc5f1
Rodrigo Chagas <dyggo.ag22@gmail.com>
2023-04-22 11:41:47 -0300

    implementando solução usando LSP - SOLID

---
+++ Practicing_SOLID_CSharp/3 - LSP/Solução/Quadrado.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practicing_SOLID_CSharp._3___LSP.Solução
+{
+    internal class Quadrado : Paralelogramo
+    {
+        public Quadrado(double altura, double largura) 
+            : base(altura, largura)
+        {
+            if (altura != largura)
+                throw new ArgumentException("Altura e largura de um quadrado devem ser iguais.");
+        }
+    }
+}
---
+++ Practicing_SOLID_CSharp/3 - LSP/Solução/Retangulo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practicing_SOLID_CSharp._3___LSP.Solução
+{
+    internal class Retangulo : Paralelogramo
+    {
+        public Retangulo(double altura, double largura) : base(altura, largura)
+        {
+        }
+    }
+}
