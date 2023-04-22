b464cf7fd6177f4534c43ce34c918472918b339a
Rodrigo Chagas <dyggo.ag22@gmail.com>
2023-04-22 11:43:20 -0300

    implementando violação usando ISP - SOLID

---
+++ Practicing_SOLID_CSharp/4 - ISP/Violação/CadastroCliente.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practicing_SOLID_CSharp._4___ISP.Violação
+{
+    internal class CadastroCliente : ICadastro
+    {
+        public void EnviarEmail()
+        {
+            //Envia email
+        }
+
+        public void SalvarBanco()
+        {
+            //salva no banco
+        }
+
+        public void ValidarDados()
+        {
+            //Valida dados
+        }
+    }
+}
---
+++ Practicing_SOLID_CSharp/4 - ISP/Violação/CadastroProduto.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practicing_SOLID_CSharp._4___ISP.Violação
+{
+    internal class CadastroProduto : ICadastro
+    {
+        public void EnviarEmail()
+        {
+            //Produto não tem email
+            throw new NotImplementedException();
+        }
+
+        public void SalvarBanco()
+        {
+            //salva no banco
+        }
+
+        public void ValidarDados()
+        {
+            //Valida dados
+        }
+    }
+}
