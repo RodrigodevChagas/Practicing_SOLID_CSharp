da179c78e0db06f8b53c9a8f63a5f9fe35ca1263
Rodrigo Chagas <dyggo.ag22@gmail.com>
2023-04-22 11:44:48 -0300

    implementando violação usando DIP - SOLID

---
+++ Practicing_SOLID_CSharp/5 - DIP/Violação/Models/Client.cs
@@ -0,0 +1,22 @@
+﻿using Practicing_SOLID_CSharp._5___DIP.Violação.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practicing_SOLID_CSharp._5___DIP.Violação.Models
+{
+    public class Client
+    {
+        public int ClienteId { get; set; }
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public string CPF { get; set; }
+        public DateTime DataCadastro { get; set; }
+
+        public bool IsValid() {
+            return CPFService.IsCPFValid(CPF) && EmailService.IsEmailValid(Email);
+        }
+    }
+}
