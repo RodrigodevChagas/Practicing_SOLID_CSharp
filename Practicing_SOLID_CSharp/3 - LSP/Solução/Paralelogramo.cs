c351da6ffe30f55e183da1b133066b0472e8bdfd
Rodrigo Chagas <dyggo.ag22@gmail.com>
2023-04-22 11:41:14 -0300

    implementando solução usando LSP - SOLID

---
+++ Practicing_SOLID_CSharp/3 - LSP/Solução/Paralelogramo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practicing_SOLID_CSharp._3___LSP.Solução
+{
+    internal class Paralelogramo
+    {
+        public Paralelogramo(double altura, double largura)
+        {
+            Altura = altura;
+            Largura = largura;
+        }
+
+        public double Altura { get; set; }
+        public double Largura { get; set; }
+        public double Area { get { return Altura * Largura; } }
+    }
+}
