5eac1bd53c9c30dbe9b0785e3a0ea0163ca12b7e
Rodrigo Chagas <dyggo.ag22@gmail.com>
2023-04-22 11:45:12 -0300

    implementando violação usando DIP - SOLID

---
+++ Practicing_SOLID_CSharp/5 - DIP/Violação/Services/ClientService.cs
@@ -0,0 +1,22 @@
+﻿using Practicing_SOLID_CSharp._5___DIP.Violação.Models;
+using Practicing_SOLID_CSharp._5___DIP.Violação.Repository;
+
+namespace Practicing_SOLID_CSharp._5___DIP.Violação.Services
+{
+    public class ClientService
+    {
+        public string AddClient(Client client) 
+        {
+            if (!client.IsValid())
+                return "Dados Inválidos!";
+
+            var repos = new ClientRepository();
+
+            repos.AddClient(client);
+
+            EmailService.SendEmail(client.Email);
+
+            return "Client successfully registered!";
+        }
+    }
+}
