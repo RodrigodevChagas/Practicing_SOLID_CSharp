532693330c8219ad7a21e1704890413e855e45c0
Rodrigo Chagas <dyggo.ag22@gmail.com>
2023-04-22 11:42:06 -0300

    implementando violação usando LSP - SOLID

---
+++ Practicing_SOLID_CSharp/3 - LSP/Violação/Quadrado.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practicing_SOLID_CSharp._3___LSP.Violação
+{
+    internal class Quadrado : Retangulo
+    {
+        public override double Altura
+        {
+            set { base.Altura = base.Largura = value; }
+        }
+
+        public override double Largura
+        {
+            set { base.Altura = base.Largura = value; }
+        }
+    }
+}
---
+++ Practicing_SOLID_CSharp/3 - LSP/Violação/Retangulo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practicing_SOLID_CSharp._3___LSP.Violação
+{
+    internal class Retangulo
+    {
+        public virtual double Altura { get; set; }
+        public virtual double Largura { get; set; }
+        public double Area { get { return Altura * Largura; } }
+    }
+}
